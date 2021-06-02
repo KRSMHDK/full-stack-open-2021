254b715bffc78219e014dc381eb6dcf9a82e2549
Kris <kris.mahadhika@gmail.com>
2021-06-02 10:54:15 +0800

    part 4

--- part4/blog-list/test/list.test.js
+++ part4/blog-list/test/list.test.js
@@ -1,57 +1,95 @@
-const listHelper = require("../utils/list_helper");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const helper = require('../test/test_helper')
+const app = require("../app");
+const Blog = require('../models/blog')
+const api = supertest(app);
 
-describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-  ];
+beforeEach(async () => {
+  await Blog.deleteMany({})
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(5);
-  });
+  const blogObjects = helper.initialBlogs
+  .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
 });
 
-describe("most likes", () => {
-  const manyBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "ggg22",
-      title: "asdasdas22",
-      author: "Easdasdsad",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 222,
-      __v: 0,
-    },
-    {
-      _id: "sad22312",
-      title: "666",
-      author: "Easdasdsadasdas",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 666,
-      __v: 0,
-    },
-  ];
 
-  test("totallikes", () => {
-    const result = listHelper.favoriteblog(manyBlog);
-    expect(result).toEqual({
-      title: "666",
-      author: "Easdasdsadasdas",
-      likes: 666,
-    });
-  });
+test('a specific note is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.title)
+  expect(contents).toContain(
+    'TDD harms architecture'
+  )
+})
+
+test('property named id is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const id = response.body.map(r => r.id)
+  expect(id).toBeDefined()
+})
+
+test('sucessfuly creates a new blog post', async () => {
+  blogTest =  {
+    title: 'leo',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 33
+  }
+
+  await api
+  .post('/api/blogs')
+  .send(blogTest)
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length +1)
+
+  const contents = blogsAtEnd.map(n => n.title)
+  expect(contents).toContain('leo')
+})
+
+test('if the likes property is missing, it will default to the value 0', async () =>{
+  blogTest = {
+    title: 'test',
+    author: 'test2',
+    url: 'https://aaaa.com/'
+  }
+
+  await api
+  .post('/api/blogs')
+  .send(blogTest)
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const likes = blogsAtEnd[blogsAtEnd.length - 1].likes
+  
+  expect(likes).toBe(0)
+})
+
+test('if the title and url missing expect 400', async () =>{
+  blogTest = {
+    author: 'test2',
+    likes: '1'
+  }
+
+  await api
+  .post('/api/blogs')
+  .send(blogTest)
+  .expect(400)
+ 
+})
+
+afterAll(() => {
+  mongoose.connection.close();
 });
