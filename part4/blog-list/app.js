2cbd7ddd70c7bdabf84c907d9149916a9cc9e8f6
Kris <kris.mahadhika@gmail.com>
2021-06-22 00:08:19 +0800

    part5

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
-
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -26,5 +27,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
-
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 module.exports = app;
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,17 +1,38 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
-blogsRouter.post("/", async(request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const body = request.body;
- 
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   if (request.body.title === undefined || request.body.url === undefined) {
-    response.status(400).end()
-    return
+    response.status(400).end();
+    return;
   }
 
   const blog = new Blog({
@@ -19,10 +40,33 @@ blogsRouter.post("/", async(request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-  })
- 
-  const savedBlog = await blog.save()
-  response.json(savedBlog)
+    user: user._id,
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.json(savedBlog);
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: body.user,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.status(201).json(updatedBlog.toJSON());
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
 });
 
 module.exports = blogsRouter;
